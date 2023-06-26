9d14c395c5e33f86aab1da5848ee011841c3884e
PedroK-Calco <132271856+PedroK-Calco@users.noreply.github.com>
2023-06-26 09:45:39 +0200

    Created a new pojo for orders
    extends product class to include an 'amount' property

---
+++ pojo/CartItem.java
@@ -0,0 +1,33 @@
+package pojo;
+
+import java.math.BigDecimal;
+
+public class CartItem extends Product{
+    private int amount;
+
+    public CartItem(String name, BigDecimal price, int hours, int amount) {
+        super(name, price, hours);
+        this.setAmount(amount);
+    }
+
+    public CartItem(Product product, int amount){
+        super(product.getName(), product.getPrice(), product.getHours());
+        this.setAmount(amount);
+    }
+
+    public CartItem clone() {return new CartItem(super.getName(), super.getPrice(), super.getHours(), this.getAmount());}
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        if (amount < 0) throw new IllegalArgumentException("Amount cannot be less than 0.");
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + getName() + "\nPrice: " + getPrice() + "\nHours: " + getHours() + "\nAmount: " + getAmount();
+    }
+}

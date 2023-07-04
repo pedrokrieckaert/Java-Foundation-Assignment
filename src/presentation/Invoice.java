220c0ec1f7f90c85b16f9f821aefd2b743f7ce12
PedroK-Calco <132271856+PedroK-Calco@users.noreply.github.com>
2023-07-04 15:10:51 +0200

    New padding method to get even distributed columns

--- src/presentation/Invoice.java
+++ src/presentation/Invoice.java
@@ -10,19 +10,29 @@ import static src.presentation.InvoiceSpacingEnum.*;
 public abstract class Invoice {
 
     public static void printCart(List<CartItem> cart) {
-        System.out.println("Photo Type" + FOUR.spaces
-                + "Price" + FOUR.spaces
-                + "Amount" + FOUR.spaces
-                + "Total Costs"
+        final int padSmall = 15;
+        final int padLarge = 30;
+
+        System.out.println(padRight("Photo Type",padLarge)
+                + padRight("Price(€)",padSmall)
+                + padRight("Amount",padSmall)
+                + padRight("Total Costs(€)",padSmall)
         );
 
         for (CartItem item : cart) {
+            BigDecimal totalCost = item.getPrice().multiply(BigDecimal.valueOf(item.getAmount()));
+
             System.out.println(
-                    item.getName() + FOUR.spaces
-                            + item.getPrice() + FOUR.spaces
-                            + item.getAmount() + FOUR.spaces
-                            + (item.getPrice().multiply(BigDecimal.valueOf(item.getAmount())))
+                    padRight(item.getName(),padLarge)
+                            + padRight(item.getPrice().toString(),padSmall)
+                            + padRight(String.valueOf(item.getAmount()),padSmall)
+                            + padRight(totalCost.toString(),padSmall)
             );
         }
+    }
+
+    private static String padRight(String s, int n) {
+        String test = String.format("%-" + n + "s", s);
+        return test;
     }
 }

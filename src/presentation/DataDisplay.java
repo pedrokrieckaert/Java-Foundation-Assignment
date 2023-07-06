831e4c6ec44eb7f15800867faccff6a0dafd96bc
PedroK-Calco <132271856+PedroK-Calco@users.noreply.github.com>
2023-07-06 11:51:04 +0200

    new class DataDisplay

---
+++ src/presentation/DataDisplay.java
@@ -0,0 +1,16 @@
+package src.presentation;
+
+public abstract class DataDisplay {
+
+    static void displayProducts() {
+
+    }
+
+    static void displayHours() {
+
+    }
+
+    static void displayCart() {
+        
+    }
+}

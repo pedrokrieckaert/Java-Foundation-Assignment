e7e5568e189749be96412fe172060fb69c7dfa4f
PedroK-Calco <132271856+PedroK-Calco@users.noreply.github.com>
2023-07-12 15:24:23 +0200

    optimized and documented unique functions of POJOs

--- src/pojo/Order.java
+++ src/pojo/Order.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Order {
     private String orderDate;
@@ -38,20 +39,25 @@ public class Order {
         this.totalHours = totalHours;
     }
 
+    /**
+     * Checks to see if the cart is already populated and returns the cart variable if it is.
+     * Otherwise, this returns a new cart to be populated.
+     * @return A new ArrayList or an existing List of CartItem
+     */
     public List<CartItem> getCart() {
-        if (this.cart == null) {
-            return new ArrayList<>();
-        } else {
-            return cart;
-        }
+        return Objects.requireNonNullElseGet(this.cart, ArrayList::new);
     }
 
-    //Create deep copy
+    /**
+     * Deep copies the parameter cart and sets this cart to the copy cart.
+     * <p><i>How many more times can I write cart.</i></p>
+     * @param source List of CartItem
+     */
     public void setCart(List<CartItem> source) {
         List<CartItem> cart = new ArrayList<>();
 
-        for (int i = 0; i < source.size(); i++) {
-            cart.add(source.get(i).clone());
+        for (CartItem cartItem : source) {
+            cart.add(cartItem.clone());
         }
         this.cart = cart;
     }
@@ -76,9 +82,13 @@ public class Order {
         return orderDate;
     }
 
+    /**
+     * Gets the current system time and date.
+     * Formats it to the appropriate DateTimeFormat and sets this variable.
+     */
     public void setOrderDate() {
         LocalDateTime now = LocalDateTime.now();
-        this.orderDate = this.DTF.format(now);
+        this.orderDate = DTF.format(now);
     }
 
     public String getPickUpDate() {
@@ -89,6 +99,10 @@ public class Order {
         this.pickUpDate = pickUpDate;
     }
 
+    /**
+     * Custom toString method to only format relevant data to the String.
+     * @return String of pickup day, date, and pick up time.
+     */
     public String pickUpDataToString() {
         return this.pickUpDay + " " + this.pickUpDate + " " + this.pickUpTime;
     }
